b1de4abdf4337dc7680e435770b022ae92c728f2
Wes Turner <wes@wrd.nu>
2016-08-05 20:00:31 -0500

    TST: pynvramutil.py: Test_nvramshowdiff(unittest.TestCase)

--- pynvramutil/pynvramutil.py
+++ pynvramutil/pynvramutil.py
@@ -126,7 +126,64 @@ def nvramshowdiff(show1, show2):
     """
 
 
-import sys
+import codecs
+import types
+import unittest
+
+import collections
+import json
+
+
+class Test_nvramshowdiff(unittest.TestCase):
+
+    def setUp(self):
+        self.conf = collections.OrderedDict()
+        self.conf['file_path'] = './wzr1750dhp-nvram_settings.2.txt'
+
+    def test_read_nvram_tuples(self):
+        file_path = self.conf['file_path']
+        with codecs.open(file_path, 'r', 'utf8') as f:
+            nvram_kviter = NvramDump.read_nvram_tuples(f)
+            self.assertTrue(hasattr(nvram_kviter, '__iter__'))
+            self.assertTrue(isinstance(nvram_kviter, types.GeneratorType))
+            nvram_kvlist = sorted(nvram_kviter, key=lambda x: x[0])
+            nvram_dict = collections.OrderedDict(nvram_kvlist)
+            self.assertGreater(len(nvram_dict), 10)
+            json_output = json.dumps(nvram_dict, indent=2)
+            print(json_output)
+            self.assertGreater(len(json_output), 10)
+        #raise Exception()  # force debug print
+
+    def test_Nvram(self):
+        file_path = self.conf['file_path']
+        with codecs.open(file_path, 'r', 'utf8') as f:
+            nvramdump = NvramDump(f)
+            self.assertTrue(nvramdump.data)
+            print(('nvramdump.data', nvramdump.data))
+
+            json_output = nvramdump.to_json(indent=2)
+            self.assertGreater(len(json_output), 10)
+            print(('nvramdump.to_json', json_output))
+
+            pretty_output = nvramdump.to_pretty()
+            self.assertGreater(len(pretty_output), 10)
+            print(('nvramdump.to_pretty','... ->'))
+            print(pretty_output)
+
+            str_output = str(nvramdump)
+            self.assertGreater(len(str_output), 10)
+            print(('nvramdump.to_str','... ->'))
+            print(str_output)
+
+            self.assertTrue(str_output == json_output)
+
+        #raise Exception()  # force debug print
+
+    def test_nvramshowdiff(self):
+        pass
+
+    def tearDown(self):
+        pass
 
 
 def main(argv=None):
